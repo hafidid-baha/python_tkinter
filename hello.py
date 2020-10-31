8529d0a61844d6c8bfa0413a396b2ef70b30ae69
hafid id-baha <hafidsoftdev@gmail.com>
2020-10-31 16:10:29 +0100

    first programme

---
+++ hello.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+# create the root window
+root = Tk()
+
+# create a label insde root window
+mLabel = Label(root,text="hello world")
+# adding label to the screen
+mLabel.pack()
+
+# keep the programme running intel the user close the app
+root.mainloop()
