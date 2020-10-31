1e17024361671b01c3fe3d2547963ab216526654
hafid id-baha <hafidsoftdev@gmail.com>
2020-10-31 16:36:51 +0100

    usign grid system instead of peck function

---
+++ grid.py
@@ -0,0 +1,14 @@
+from tkinter import *
+
+# create the root window
+root = Tk()
+
+# create a label insde root window
+username = Label(root,text="hafid id-baha")
+userrole = Label(root,text="admin user")
+# using grid instead of pack
+username.grid(row=0,column=1)
+userrole.grid(row=1,column=0)
+
+# keep the programme running intel the user close the app
+root.mainloop()
