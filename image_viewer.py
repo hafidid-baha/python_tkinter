3e00e64c5549582d2630ddc1fd6ab2c8cb932109
hafid id-baha <hafidsoftdev@gmail.com>
2020-11-05 11:40:12 +0100

    create a status bar

--- image_viewer.py
+++ image_viewer.py
@@ -2,9 +2,11 @@ from tkinter import *
 from PIL import Image, ImageTk
 # create the root window
 root = Tk()
-root.title = "learn python with tkinter"
+root.title = "learn python"
 # chosing app icon
 root.iconbitmap('img/spider.ico')
+
+counter = 0
 
 # get the image object
 image1 = ImageTk.PhotoImage(Image.open("img/1.jpg"))
@@ -16,8 +18,10 @@ image_list = [image1, image2, image3, image4]
 # show the image on the label
 label = Label(image=image_list[0])
 label.grid(row=0, column=0, columnspan=3)
-
-counter = 0
+status_label = Label(
+    root, text=f"image {counter+1} / {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
+status_label.grid(row=2, column=0, columnspan=3,
+                  sticky=W+E)
 
 
 def replace_image(index):
@@ -33,19 +37,33 @@ def next():
     if counter < len(image_list)-1:
         counter = counter + 1
     replace_image(counter)
+    upadate_status()
 
 
 def prev():
     global counter
     global label
-    if counter >= 0:
+    if counter > 0:
         counter = counter - 1
     replace_image(counter)
+    upadate_status()
+
+
+def upadate_status():
+    global counter
+    global status_label
+    global image_list
+    status_label.grid_forget()
+    status_label = Label(
+        root, text=f"image {counter+1} / {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
+    status_label.grid(row=2, column=0, columnspan=3,
+                      sticky=W+E)
 
 
 back_btn = Button(root, text="<<", command=lambda: prev())
 exit_btn = Button(root, text="Exit", command=root.quit)
 next_btn = Button(root, text=">>", command=lambda: next())
+
 
 back_btn.grid(row=1, column=0)
 exit_btn.grid(row=1, column=1)
